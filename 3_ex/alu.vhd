2e5441311395ee6fd36680b110df8902e7d2b20f
Nicole Dai Prà <nicoledaipra96@gmail.com>
2020-09-25 17:27:15 +0200

    alu tested with registers

--- 3_ex/alu.vhd
+++ 3_ex/alu.vhd
@@ -1,33 +1,35 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- ALU that encapsulate the computing units and the selector of the result
+-- ALU that encapsulate the computing units, the comparator and the selector of the result
 
 entity alu is
 	port ( 
-		clk: in std_logic;
-		rst: in std_logic;
-
-    	op_type: in std_logic_vector(1 downto 0);
-    	op_sign: in std_logic; 							-- 1 if the operands are signed, 0 otherwise
-
     	-- operands
     	a_adder: in std_logic_vector(31 downto 0);
     	b_adder: in std_logic_vector(31 downto 0);
-    	a_mult: in std_logic_vector(31 downto 0);
-    	b_mult: in std_logic_vector(31 downto 0);
+    	a_mult: in std_logic_vector(63 downto 0);
+    	a_neg_mult: in std_logic_vector(63 downto 0);
+    	b_mult: in std_logic_vector(2 downto 0);
+    	b10_1_mult: in std_logic_vector(2 downto 0);
     	a_shift: in std_logic_vector(31 downto 0);
-    	b_shift: in std_logic_vector(31 downto 0);
-    	a_log: in std_logic_vector(31 downto 0);
-    	b_log: in std_logic_vector(31 downto 0);
+    	b_shift: in std_logic_vector(4 downto 0);
+    	mul_feedback: in std_logic_vector(63 downto 0);
 
-    	sub_add: in std_logic;							-- 1 if it is a subtraction, 0 otherwise
+    	-- control signals
+    	sub_add: in std_logic;						-- 1 if it is a subtraction, 0 otherwise
     	shift_type: in std_logic_vector(3 downto 0);
     	log_type: in std_logic_vector(3 downto 0);
-    	
+    	op_type: in std_logic_vector(1 downto 0);	--00: add/sub, 01: mul, 10: shift/rot, 11: log
+    	op_sign: in std_logic; 						-- 1 if the operands are signed, 0 otherwise
+    	it: in std_logic_vector(3 downto 0);		-- iteration
+    	neg: in std_logic;							-- used to negate a before actually multiplying
+
+    	-- outputs
     	alu_out_high: out std_logic_vector(31 downto 0);
     	alu_out_low: out std_logic_vector(31 downto 0);
-    	alu_flags: out std_logic_vector(2 downto 0); 	-- Z: zero flag, V: overflow flag, S: sign flag
+    	alu_flags: out std_logic_vector(2 downto 0); 
+    	a_neg_out: out std_logic_vector(63 downto 0);	
 		le: out std_logic; -- less than or equal
 		lt: out std_logic; -- less than
 		ge: out std_logic; -- greater than or equal
@@ -53,16 +55,18 @@ architecture beh of alu is
 		);
 	end component p4_adder;
 
-	component boothmul is
-		generic (
-		    N: integer := 32
-		);
+	component mul is
 		port (
-		    a: in std_logic_vector(N-1 downto 0);
-		    b: in std_logic_vector(N-1 downto 0);
-		    y: out std_logic_vector(2*N-1 downto 0)
+			a: in std_logic_vector(63 downto 0);
+			a_neg: in std_logic_vector(63 downto 0);
+			b10_1: in std_logic_vector(2 downto 0);
+			b: in std_logic_vector(2 downto 0);
+			it: in std_logic_vector(3 downto 0);
+			neg: in std_logic; -- used to negate a before actually multiplying
+			mul_feedback: in std_logic_vector(63 downto 0);
+			res: out std_logic_vector(63 downto 0)
 		);
-	end component boothmul;
+	end component mul;
 
 	component shifter_t2 is
 	    port (
@@ -91,21 +95,19 @@ architecture beh of alu is
 			N: integer := 32
 		);
 	    port (	
-	    	clk: in std_logic;
-	    	rst: in std_logic;
-	    	a: std_logic_vector(N-1 downto 0);
-	    	b: std_logic_vector(N-1 downto 0);
-	    	op_type: in std_logic_vector(1 downto 0);
-	    	op_sign: in std_logic;                          -- 1 if the operands are signed, 0 otherwise
-	    	sub_add: std_logic; 							
+	    	a: in std_logic;	-- MSB of a
+	    	b: in std_logic;	-- MSB of b
+	    	op_type: in std_logic_vector(1 downto 0); -- identifies which unit is computing the result
+	    	op_sign: in std_logic; 	-- 1 if the operands are signed, 0 otherwise
+	    	sub_add: std_logic;		-- 1 if it is a subtraction, 0 otherwise
 	    	adder_out: in std_logic_vector(N-1 downto 0);
 	    	adder_cout: in std_logic;
 	    	mul_out: in std_logic_vector(2*N-1 downto 0);
 	    	shifter_out: in std_logic_vector(N-1 downto 0);
 	    	logicals_out: in std_logic_vector(N-1 downto 0);
-	    	alu_sel_out_high: out std_logic_vector(N-1 downto 0);
-	    	alu_sel_out_low: out std_logic_vector(N-1 downto 0);
-	    	alu_flags: out std_logic_vector(2 downto 0) 	-- Z: zero flag, V: overflow flag, S: sign flag 
+	    	alu_sel_out_high: out std_logic_vector(N-1 downto 0); -- upper part (63 downto 32) of the result used only for the multiply
+	    	alu_sel_out_low: out std_logic_vector(N-1 downto 0); -- lower part (31 downto 0) of the result
+	    	alu_flags: out std_logic_vector(2 downto 0) 	
 	    );
 	end component alu_out_selector;
 
@@ -131,6 +133,7 @@ architecture beh of alu is
 	signal adder_out, shifter_out, logicals_out: std_logic_vector(31 downto 0);
 	signal mul_out: std_logic_vector(63 downto 0);
 	signal adder_cout: std_logic;
+	signal alu_out_high_int, alu_out_low_int: std_logic_vector(31 downto 0); 
 
 begin
 	
@@ -147,20 +150,22 @@ begin
 			carries => open
 		);
 
-	mul: boothmul
-		generic map (
-			N => 32
-		)
+	mult: mul
 		port map (
 			a => a_mult,
+			a_neg => a_neg_mult,
+			b10_1 => b10_1_mult,
 			b => b_mult,
-			y => mul_out
+			it => it,
+			neg => neg,
+			mul_feedback => mul_feedback,
+			res => mul_out
 		);
 
 	shifter: shifter_t2
 		port map (
 			data_in => a_shift,
-			shift => b_shift(4 downto 0),
+			shift => b_shift,
 			shift_type => shift_type,
 			data_out => shifter_out
 		);
@@ -170,8 +175,8 @@ begin
 			N => 32
 		)
 		port map (
-			a => a_log,
-			b => b_log,
+			a => a_adder,
+			b => b_adder,
 			sel => log_type,
 			res => logicals_out
 		);
@@ -181,10 +186,8 @@ begin
 			N => 32
 		)
 		port map (
-			clk => clk,
-			rst => rst,
-			a => a_adder,
-			b => b_adder,
+			a => a_adder(31),
+			b => b_adder(31),
 			sub_add => sub_add,
 			op_type => op_type,
 			op_sign => op_sign,
@@ -193,8 +196,8 @@ begin
 			mul_out => mul_out,
 			shifter_out => shifter_out,
 			logicals_out => logicals_out,
-			alu_sel_out_high => alu_out_high,
-			alu_sel_out_low => alu_out_low,
+			alu_sel_out_high => alu_out_high_int,
+			alu_sel_out_low => alu_out_low_int,
 			alu_flags => alu_flags
 		);
 
@@ -216,5 +219,9 @@ begin
 			ne => ne
 		);
 
+	alu_out_high <= alu_out_high_int;
+	alu_out_low <= alu_out_low_int;
+
+	a_neg_out <= alu_out_high_int & alu_out_low_int;
 
 end beh;
--- 3_ex/alu_out_selector.vhd
+++ 3_ex/alu_out_selector.vhd
@@ -4,54 +4,43 @@ use ieee.math_real.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_misc.all;
 
--- Unit that selects the output of the ALU based on the operation's type and that set the flags related
+-- Unit that selects the output of the ALU based on the operation's type. It also sets the flags related
 -- to the output. 
--- The flags' register has three fields: 
+
+-- The alu_flag has three fields: 
 -- alu_flags(2): Zero flag, set to 1 if the result is 0, to 0 otherwise
 -- alu_flags(1): Overflow flag, set to 1 is an overflow occured, to 0 otherwise
--- alu_flags(0): Sign flag, set to 1 if the operands are signed, to 0 otherwise
+-- alu_flags(0): Sign flag, set to 1 if the result is negative, 0 otherwise
+
+-- The result is produced on 32 bits except for the multiply that computes a 64 bits result.
+-- In that case, it is split and returned by using alu_out_sel_high and alu_out_sel_high.
 
 entity alu_out_selector is
 	generic(
 		N: integer := 32
 	);
     port (	
-    	clk: in std_logic;
-    	rst: in std_logic;
-    	a: in std_logic_vector(N-1 downto 0);
-    	b: in std_logic_vector(N-1 downto 0);
-    	op_type: in std_logic_vector(1 downto 0);
-    	op_sign: in std_logic; 							-- 1 if the operands are signed, 0 otherwise
-    	sub_add: std_logic;
+    	a: in std_logic;	-- MSB of a
+    	b: in std_logic;	-- MSB of b
+    	op_type: in std_logic_vector(1 downto 0); -- identifies which unit is computing the result
+    	op_sign: in std_logic; 	-- 1 if the operands are signed, 0 otherwise
+    	sub_add: std_logic;		-- 1 if it is a subtraction, 0 otherwise
     	adder_out: in std_logic_vector(N-1 downto 0);
     	adder_cout: in std_logic;
     	mul_out: in std_logic_vector(2*N-1 downto 0);
     	shifter_out: in std_logic_vector(N-1 downto 0);
     	logicals_out: in std_logic_vector(N-1 downto 0);
-    	alu_sel_out_high: out std_logic_vector(N-1 downto 0);
-    	alu_sel_out_low: out std_logic_vector(N-1 downto 0);
-    	alu_flags: out std_logic_vector(2 downto 0) 	-- Z: zero flag, V: overflow flag, S: sign flag
+    	alu_sel_out_high: out std_logic_vector(N-1 downto 0); -- upper part (63 downto 32) of the result used only for the multiply
+    	alu_sel_out_low: out std_logic_vector(N-1 downto 0); -- lower part (31 downto 0) of the result
+    	alu_flags: out std_logic_vector(2 downto 0) 	
     );
 end alu_out_selector;
 
-architecture Behavioral of alu_out_selector is
-
-	signal curr_flags, next_flags: std_logic_vector(2 downto 0);
+architecture behavioral of alu_out_selector is
 
 begin
 
-	reg_p: process (clk, rst)
-		begin
-			if (clk = '1' and clk'event) then
-				if (rst = '0') then
-					curr_flags <= (others => '0');
-				else
-					curr_flags <= next_flags;
-				end if;
-			end if;
-		end process reg_p;
-
-	sel_p: process (curr_flags, op_type, op_sign, sub_add, adder_out, adder_cout, mul_out, shifter_out, logicals_out)
+	sel_p: process (op_type, op_sign, sub_add, adder_out, adder_cout, mul_out, shifter_out, logicals_out)
 		  variable overflow: std_logic;
 		  
 		begin
@@ -61,42 +50,44 @@ begin
 			-- 01: multiplier
 			-- 10: shift/rotate
 			-- 11: logicals
-			
-			next_flags <= curr_flags;
+
 			alu_sel_out_high <= (others => '0');
 			
 			case op_type is
 				when "00" => 
 				    
-				    overflow:= (not(a(N-1)) and not(b(N-1)) and adder_out(N-1)) or (a(N-1) and b(N-1) and not(adder_out(N-1)));
-					overflow:= overflow or (sub_add and (adder_cout xor adder_out(N-1)));
+				    -- check if an overflow occured 
+				    overflow := (not(a) and not(b) and adder_out(N-1)) or (a and b and not(adder_out(N-1)));
+					overflow := overflow or (sub_add and (adder_cout xor adder_out(N-1)));
 					alu_sel_out_low <= adder_out;
-					if (op_sign = '1') then
-						next_flags <= not(or_reduce(adder_out)) & overflow & op_sign;
-					else
-						-- DLX instructions do not produce overflow with unsigned numbers
-						next_flags <= not(or_reduce(adder_out)) & '0' & op_sign; 
-					end if;	
+
+					-- add/sub instructions do not produce overflow with unsigned numbers
+					alu_flags <= not(or_reduce(adder_out)) & (op_sign and overflow) & (op_sign and adder_out(N-1));
 
 				when "01" => 
 					alu_sel_out_low <= mul_out(N-1 downto 0);
 					alu_sel_out_high  <= mul_out(2*N-1 downto N);
-					next_flags <= not(or_reduce(mul_out)) & '0' & op_sign;
+
+					-- MUL is performed with signed numbers only
+					alu_flags <= not(or_reduce(mul_out)) & '0' & mul_out(N-1);
 
 				when "10" => 
 					alu_sel_out_low <= shifter_out;
-					next_flags <= not(or_reduce(shifter_out)) & '0' & op_sign;
+
+					-- shift operations are not supposed to produce overflow
+					alu_flags <= not(or_reduce(shifter_out)) & '0' & (op_sign and shifter_out(N-1));
 
 				when "11" => 
 					alu_sel_out_low <= logicals_out;
-					next_flags <= not(or_reduce(logicals_out)) & '0' & '0';
+
+					-- logicals are not supposed to produce overfow
+					-- logicals work with pure binary numbers so the sign flag is set to 0 by default
+					alu_flags <= not(or_reduce(logicals_out)) & '0' & '0';
 				
 				when others => 
 					alu_sel_out_low <= (others => '0');
+					alu_flags <= (others => '0');
 					
 			end case;
 		end process sel_p;
-
-		alu_flags <= curr_flags;
-
-end Behavioral;
+end behavioral;
